b806e666f6c72ea0bd859e25ac008dcdcb6f71fd
이승재 <158141455+think92@users.noreply.github.com>
2024-04-26 17:43:22 +0900

    commit
    commit

--- react/react웹개발시작하기/study2/src/App.js
+++ react/react웹개발시작하기/study2/src/App.js
@@ -17,29 +17,60 @@ function App() {
   const [hand, setHand] = useState(INITIAL_VALUE);
   const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
   const [gameHistory, setGameHistory] = useState([]);
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
 
   const HandButtonClick = (nextHand) => {
     const nextOtherHand = generateRandomHand();
     const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
     setGameHistory([...gameHistory, nextHistoryItem]);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
   const handleClearClick = () => {
     setHand(INITIAL_VALUE);
     setOtherHand(INITIAL_VALUE);
     setGameHistory([]);
+    setScore(0);
+    setOtherScore(0);
+    setBet(0);
   };
+
+  const handleBetChange = (e) => {
+    let num = Number(e.target.value);
+    if (num > 9) num %= 10;
+    if (num < 1) num = 1;
+    num = Math.floor(num);
+    setBet(num);
+  };
+
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
+      <div>
+        {score} : {otherScore}
+      </div>
       <p>{getResult(hand, otherHand)}</p>
       <div>
         <HandIcon value={hand} />
         VS
         <HandIcon value={otherHand} />
       </div>
+      <div>
+        <input
+          type="number"
+          value={bet}
+          min={1}
+          max={9}
+          onChange={handleBetChange}
+        ></input>
+      </div>
+      <p>승부 기록 : {gameHistory.join(", ")}</p>
       <div>
         <HandButton value="rock" onClick={HandButtonClick} />
         <HandButton value="scissor" onClick={HandButtonClick} />
